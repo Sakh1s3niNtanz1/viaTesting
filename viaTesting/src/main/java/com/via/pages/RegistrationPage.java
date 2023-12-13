d8539c4a37161c5878056dca415565e88c9e4bd6
PhilaniMagwaza <149668645+PhilaniMagwaza@users.noreply.github.com>
2023-12-13 09:06:49 +0200

    Update RegistrationPage.java

--- viaTesting/src/main/java/com/via/pages/RegistrationPage.java
+++ viaTesting/src/main/java/com/via/pages/RegistrationPage.java
@@ -1,5 +1,58 @@
 package com.via.pages;
 
-public class RegistrationPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.via.base.ViaBase;
+
+public class RegistrationPage extends ViaBase {
+	
+	public WebElement getSignUp() {
+		
+		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		
+		return driver.findElement(By.xpath("//div[@id=\'SignInContent\']/div[1]/div[1]/div[2]/span/label/span"));
+	}
+	
+	public String getTitle()
+	{
+		return driver.getTitle();
+	}
+	
+	
+	public WebElement getCreatAccountBtn() {
+		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		driver.findElement(By.xpath("//div[@id=\'SignInContent\']/div[1]/div[1]/div[2]/span/label/span")).click();
+		return driver.findElement(By.id("signUpValidate"));
+		}
+	
+	public WebElement getEmailField() {
+		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		driver.findElement(By.xpath("//div[@id=\'SignInContent\']/div[1]/div[1]/div[2]/span/label/span")).click();
+		
+		return driver.findElement(By.id("emailIdSignUp"));
+	}
+	
+	public WebElement getPasswordField() {
+		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		driver.findElement(By.xpath("//div[@id=\'SignInContent\']/div[1]/div[1]/div[2]/span/label/span")).click();
+		
+		return driver.findElement(By.id("passwordSignUp"));
+	}
+	
+	public WebElement getNameField() {
+		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		driver.findElement(By.xpath("//div[@id=\'SignInContent\']/div[1]/div[1]/div[2]/span/label/span")).click();
+		
+		return driver.findElement(By.id("nameSignUp"));
+	}
+	
+	public WebElement getNumberField() {
+		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		driver.findElement(By.xpath("//div[@id=\'SignInContent\']/div[1]/div[1]/div[2]/span/label/span")).click();
+		
+		return driver.findElement(By.id("mobileNoSignUp"));
+	}
+	
 
 }
