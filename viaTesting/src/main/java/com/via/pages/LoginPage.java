afa0b9e6b23e2612aeb626799beb1229cf6b82d7
PhilaniMagwaza <149668645+PhilaniMagwaza@users.noreply.github.com>
2023-12-13 09:31:19 +0200

    Update LoginPage.java

--- viaTesting/src/main/java/com/via/pages/LoginPage.java
+++ viaTesting/src/main/java/com/via/pages/LoginPage.java
@@ -19,18 +19,18 @@ public class LoginPage extends ViaBase {
 	
 	
 	public WebElement getUsernameField() {
-		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
 			
 		return driver.findElement(By.id("loginIdText"));
 		}
 	public WebElement getPasswordField() {
-		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
 		
 		return driver.findElement(By.id("passwordText"));
 	}
 	public WebElement getForgotPassword() {
-		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
 		
-		return driver.findElement(By.xpath("//div[@id=\"SignInContent\"]/div[1]/div[2]/div[1]/div/div[2]/label"));
+		return driver.findElement(By.xpath("//div[@id='SignInContent']/div[1]/div[2]/div[1]/div/div[2]/label"));
 	}
 }
