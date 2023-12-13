013e2dfbe570d8fc97e8516c56f28157449d852b
PhilaniMagwaza <149668645+PhilaniMagwaza@users.noreply.github.com>
2023-12-13 09:06:16 +0200

    Update LoginPage.java

--- viaTesting/src/main/java/com/via/pages/LoginPage.java
+++ viaTesting/src/main/java/com/via/pages/LoginPage.java
@@ -1,5 +1,36 @@
 package com.via.pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import com.via.base.ViaBase;
 
+public class LoginPage extends ViaBase {
+	
+	public WebElement getSigninBtn() {
+		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		
+		return driver.findElement(By.id("loginValidate"));
+	}
+	
+//	public boolean validateSinginBtn() {
+//		return driver.findElement(By.xpath("//input[@id='loginValidate']")).isDisplayed();
+//
+//	}
+	
+	
+	public WebElement getUsernameField() {
+		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+			
+		return driver.findElement(By.id("loginIdText"));
+		}
+	public WebElement getPasswordField() {
+		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		
+		return driver.findElement(By.id("passwordText"));
+	}
+	public WebElement getForgotPassword() {
+		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		
+		return driver.findElement(By.xpath("//div[@id=\"SignInContent\"]/div[1]/div[2]/div[1]/div/div[2]/label"));
+	}
 }
