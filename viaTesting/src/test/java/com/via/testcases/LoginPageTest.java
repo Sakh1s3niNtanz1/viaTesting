6691e35927058c3c417c645782e22969afdf3cdc
PhilaniMagwaza <149668645+PhilaniMagwaza@users.noreply.github.com>
2023-12-13 09:05:34 +0200

    Update LoginPageTest.java

--- viaTesting/src/test/java/com/via/testcases/LoginPageTest.java
+++ viaTesting/src/test/java/com/via/testcases/LoginPageTest.java
@@ -1,5 +1,79 @@
 package com.via.testcases;
 
-public class LoginPageTest {
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.via.pages.RegistrationPage;
+
+public class RegistrationPageTest {
+	
+	
+	RegistrationPage rp = new RegistrationPage();
+	
+	@BeforeMethod
+	public void setUpLoginTest() {
+		rp.setUp();
+	}
+	
+	@Test
+	public void clickSigninTest() {
+		rp.getSignUp().click();
+	
+	}
+	
+	@Test
+	public void validateTitleFieldTest()
+	{
+		String expTitle = "Book Flights, Hotels, Bus and Holiday Packages Online - Via.com";
+		String actTitle = rp.getTitle();
+		Assert.assertEquals(actTitle,expTitle);
+	}
+	
+	@Test
+	public void validateCreatAccountBtnTest() {
+		Assert.assertTrue(rp.getCreatAccountBtn().isDisplayed());
+	
+	}
+	
+	@Test
+	public void validateEmailFieldTest() {
+		Assert.assertTrue(rp.getEmailField().isDisplayed());
+	
+	}
+	
+	@Test
+	public void validatePasswordFieldTest() {
+		Assert.assertTrue(rp.getPasswordField().isDisplayed());
+	
+	}
+	
+	@Test
+	public void validateNameFieldTest() {
+		Assert.assertTrue(rp.getNameField().isDisplayed());
+	
+	}
+	
+	@Test
+	public void validateNumberFieldTest() {
+		Assert.assertTrue(rp.getNumberField().isDisplayed());
+	
+	}
+	
+	@Test
+	public void validateRegistrationTest() {
+		rp.getEmailField().sendKeys("magwaza749@gmail.com");
+		rp.getPasswordField().sendKeys("Philani@123");
+		rp.getNameField().sendKeys("Philani Magwaza");
+		rp.getNumberField().sendKeys("0719270404");
+		rp.getCreatAccountBtn().click();
+		
+	}
+	
+	@AfterMethod
+	public void closeSetUpLogin() {
+		rp.closeSetUp();
+	}
 
 }
