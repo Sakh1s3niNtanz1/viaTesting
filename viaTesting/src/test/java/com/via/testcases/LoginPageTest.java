d7377527c45fb1b36043f1dc4571c1913a6e5d63
PhilaniMagwaza <149668645+PhilaniMagwaza@users.noreply.github.com>
2023-12-13 09:10:03 +0200

    Update LoginPageTest.java

--- viaTesting/src/test/java/com/via/testcases/LoginPageTest.java
+++ viaTesting/src/test/java/com/via/testcases/LoginPageTest.java
@@ -5,75 +5,48 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.via.pages.RegistrationPage;
+import com.via.pages.LoginPage;
 
-public class RegistrationPageTest {
+public class LoginPageTest {
 	
-	
-	RegistrationPage rp = new RegistrationPage();
-	
+	LoginPage lp = new LoginPage();
 	@BeforeMethod
-	public void setUpLoginTest() {
-		rp.setUp();
+	public void setUpLogin() {
+		lp.setUp();
 	}
-	
 	@Test
-	public void clickSigninTest() {
-		rp.getSignUp().click();
-	
-	}
-	
-	@Test
-	public void validateTitleFieldTest()
-	{
-		String expTitle = "Book Flights, Hotels, Bus and Holiday Packages Online - Via.com";
-		String actTitle = rp.getTitle();
-		Assert.assertEquals(actTitle,expTitle);
-	}
-	
-	@Test
-	public void validateCreatAccountBtnTest() {
-		Assert.assertTrue(rp.getCreatAccountBtn().isDisplayed());
+	public void validateSigninBtn() {
+		Assert.assertTrue(lp.getSigninBtn().isDisplayed());
 	
 	}
 	
-	@Test
-	public void validateEmailFieldTest() {
-		Assert.assertTrue(rp.getEmailField().isDisplayed());
-	
+	@Test 
+	public void validateUsernameField() {
+		Assert.assertTrue(lp.getUsernameField().isDisplayed());
 	}
 	
 	@Test
-	public void validatePasswordFieldTest() {
-		Assert.assertTrue(rp.getPasswordField().isDisplayed());
-	
-	}
-	
-	@Test
-	public void validateNameFieldTest() {
-		Assert.assertTrue(rp.getNameField().isDisplayed());
-	
-	}
-	
-	@Test
-	public void validateNumberFieldTest() {
-		Assert.assertTrue(rp.getNumberField().isDisplayed());
-	
+	public void validatePasswordField() {
+		Assert.assertTrue(lp.getPasswordField().isDisplayed());
 	}
 	
 	@Test
 	public void validateRegistrationTest() {
-		rp.getEmailField().sendKeys("magwaza749@gmail.com");
-		rp.getPasswordField().sendKeys("Philani@123");
-		rp.getNameField().sendKeys("Philani Magwaza");
-		rp.getNumberField().sendKeys("0719270404");
-		rp.getCreatAccountBtn().click();
-		
+		lp.getUsernameField().sendKeys("magwaza749@gmail.com");
+		lp.getPasswordField().sendKeys("Philani@123");
+	}
+	
+	@Test
+	public void validateForgetPasswordTest() {
+		Assert.assertTrue(lp.getForgotPassword().isDisplayed());
+	
 	}
 	
 	@AfterMethod
 	public void closeSetUpLogin() {
-		rp.closeSetUp();
+		lp.closeSetUp();
 	}
+	
+	
 
 }
