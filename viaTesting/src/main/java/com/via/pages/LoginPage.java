701531fc340c535f10a820e1cc811f3ea2cc9c0d
PhilaniMagwaza <149668645+PhilaniMagwaza@users.noreply.github.com>
2023-12-14 14:47:26 +0200

    Update LoginPage.java

--- viaTesting/src/main/java/com/via/pages/LoginPage.java
+++ viaTesting/src/main/java/com/via/pages/LoginPage.java
@@ -1,10 +1,17 @@
 package com.via.pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.via.base.ViaBase;
 
 public class LoginPage extends ViaBase {
+	
+	WebDriverWait wait;
 	
 	public WebElement getSigninBtn() {
 		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
@@ -16,6 +23,15 @@ public class LoginPage extends ViaBase {
 //		return driver.findElement(By.xpath("//input[@id='loginValidate']")).isDisplayed();
 //
 //	}
+	
+	public WebElement clickSigninBtn() {
+		driver.findElement(By.xpath("//div[@id=\"SignIn\"]/div")).click();
+		
+		return driver.findElement(By.id("loginValidate"));
+		
+
+	}
+	
 	
 	
 	public WebElement getUsernameField() {
@@ -32,5 +48,27 @@ public class LoginPage extends ViaBase {
 		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
 		
 		return driver.findElement(By.xpath("//div[@id='SignInContent']/div[1]/div[2]/div[1]/div/div[2]/label"));
+	}
+	
+	public WebElement getUsernameErrorMsg() {
+		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
+		
+		return driver.findElement(By.xpath("//div[@class=\"qtip-content\"]"));
+	}
+	
+	public WebElement getPasswordErrorMsg() {
+		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
+		
+		return driver.findElement(By.id("qtip-6-content"));
+	}
+	
+	public WebElement getWrongCredentialsErrorMsg() {
+		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
+		return driver.findElement(By.xpath("//div[@id='viaAlert']/div/div"));
+	}
+	
+	public void waitForEl(WebElement el) {
+		wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(el));
 	}
 }
