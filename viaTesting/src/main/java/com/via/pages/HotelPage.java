8df1364dc3c76ad3d8e5ff51299ed2f8890989d7
sikelela jona <48279605+SikelelaJona@users.noreply.github.com>
2023-12-14 14:57:50 +0200

    Update HotelPage.java
    2nd Hotel page commit for logic layer

--- viaTesting/src/main/java/com/via/pages/HotelPage.java
+++ viaTesting/src/main/java/com/via/pages/HotelPage.java
@@ -3,9 +3,11 @@ package com.via.pages;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -13,19 +15,15 @@ import com.via.base.ViaBase;
 
 public class HotelPage extends ViaBase{
 
-/*
- * 	WebDriver driver = new ChromeDriver();
-	
-	public void setUp()
-	{
-		driver.get("https://in.via.com/hotels");
-		
-	}
- */
+	Actions act;
 	
 	private WebElement findElement(By locator)
 	{
 		return driver.findElement(locator);
+	}
+	public boolean getHotelPagePresent()
+	{
+		return true;
 	}
 	
 	
@@ -33,18 +31,24 @@ public class HotelPage extends ViaBase{
 	{
 		return driver.findElement(By.id("destination"));
 	}
-	
+	public WebElement getSearchScrollBar()
+	{
+		return driver.findElement(By.xpath("//ul[@id=\"ui-id-1\"]"));
+		
+	}
 	public WebElement getCheckInDate()
 	{
 		return driver.findElement(By.id("checkIn"));
 	}
 	public WebElement getCheckOutDate()
 	{
-		return driver.findElement(By.id("csheckOut"));
+		return driver.findElement(By.id("checkOut"));
 	}
+
 	public WebElement getCheckInDateButton()
 	{
-		return driver.findElement(By.xpath("//*[@id=\"depart-cal\"]/div[3]/div[2]/div[4]/div[6]"));
+		return driver.findElement(By.xpath("//div[@id='depart-cal']/div[3]/div[2]/div[5]/div[4]"));
+		
 	}
 	
 	public WebElement getCheckOutDateButton()
@@ -52,20 +56,76 @@ public class HotelPage extends ViaBase{
 		return driver.findElement(By.xpath("//*[@id=\"return-cal\"]/div[4]/div[2]/div[4]/div[5]"));
 	}
 	
-	public WebElement getAdultsInput()
+	public WebElement getRoomsMenu()
 	{
-		return driver.findElement(By.id(""));
+		return driver.findElement(By.xpath("//div[3]/div[3]/div/form/div[3]/div/div[4]/div"));
 	}
 	
-	public WebElement getChildrenInput()
+	public WebElement getCancelButton()
 	{
-		return driver.findElement(By.id(""));
+		return driver.findElement(By.xpath("//div[3]/div[3]/div/form/div[3]/div/div[5]/div[3]/div[3]"));
 	}
 	
-	public WebElement getChildAgeInput(int index)
+	public WebElement getDoneButton()
 	{
-		return driver.findElement(By.id("" +index));
+		return driver.findElement(By.xpath("//div[3]/div[3]/div/form/div[3]/div/div[5]/div[3]/div[2]"));
 	}
+	
+	
+	public WebElement getAdultsMinusInput()
+	{
+		return driver.findElement(By.xpath("//div[@id='room1']/div[2]/div/div[1]"));
+		 
+	}
+	public WebElement getAdultsPlusInput()
+	{
+		return driver.findElement(By.xpath("//div[@id='room1']/div[2]/div/div[3]"));
+		 
+	}
+	
+	public WebElement getChildrensMinusInput()
+	{
+		return driver.findElement(By.xpath("//div[@id='room1']/div[3]/div/div[1]"));
+	}
+	
+	public WebElement getChildrensPlusInput()
+	{
+		return driver.findElement(By.xpath("//div[@id='room1']/div[3]/div/div[3]"));
+	}
+	
+	public WebElement getChildAgeInput()
+	{
+		return driver.findElement(By.xpath("//div[@id='room1childAge1']/label[2]/select"));
+	}
+	
+	public WebElement getRemoveAdditionalRoomButton()
+	{
+		return driver.findElement(By.xpath("//div[@id='room2']/div[1]/span[2]"));
+	}
+	
+	
+	public WebElement getAdditionalRooms()
+	{
+		return driver.findElement(By.xpath("//div[3]/div[3]/div/form/div[3]/div/div[5]/div[3]/div[1]"));
+	}
+	
+	public WebElement getNationalityInput()
+	{
+		return driver.findElement(By.xpath("//select[@id='nationalityCountry']"));
+	}
+	public WebElement getResidencyInput()
+	{
+		return driver.findElement(By.xpath("//select[@id='residenceCountry']"));
+	}
+	public WebElement getIsolationCheckBox()
+	{
+		return driver.findElement(By.xpath("//div[3]/div[3]/div/form/div[3]/div/div[10]/div[1]/div/label"));
+	}
+	
+
+	
+	
+	
 	
 	
 	
@@ -76,24 +136,52 @@ public class HotelPage extends ViaBase{
 	
 	public WebElement getSearchButton()
 	{
-		return driver.findElement(By.id(""));
+		return driver.findElement(By.xpath("//div[3]/div[3]/div/form/div[3]/div/div[10]/div[3]"));
 	}
 	
-	
+	public void setNavigateToHotels()
+	{
+		driver.findElement(By.xpath("//div[@id='Hotels']")).click();
+	}
 	public void setDestination (String destination)
 	{
 		getDestinationInput().sendKeys(destination);
 	}
 	
-	public void setCheckInDate(String date)
+	public void setSearchScrollBar()
 	{
-		getCheckInDate().sendKeys(date);
+		getSearchScrollBar().isDisplayed();
 	}
 	
-	public void setCheckOutDate(String date)
+	public void setCheckInDate()
 	{
-		getCheckInDate().sendKeys(date);
+		getCheckInDate().click();
+		driver.findElement(By.xpath("//div[@id='depart-cal']/div[3]/div[2]/div[5]/div[4]")).click();
+		driver.findElement(By.xpath("//span[@id='vc-close']")).click();
 	}
+	
+	public void setCheckOutDate()
+	{
+		getCheckOutDate().click();
+		driver.findElement(By.xpath("//div[@id='return-cal']/div[3]/div[2]/div[5]/div[7]")).click();
+		driver.findElement(By.xpath("//span[@id='vc-close']")).click();
+	}
+	
+	public void setRoomsMenu()
+	{
+		getRoomsMenu().click();
+		
+	}
+	public void setDoneButton()
+	{
+		getDoneButton().click();
+	}
+	
+	public void setCancelButton ()
+	{
+		getCancelButton().click();
+	}
+	
 	public void selectCheckInDate(String date)
 	{
 		getCheckInDateButton().click();
@@ -103,35 +191,76 @@ public class HotelPage extends ViaBase{
 		getCheckOutDateButton().click();
 	}
 	
-	public void enterRoomDetails (int adults, int children, int [] childAges)
+	public void setAdultsMinusInput()
 	{
-		getAdultsInput().clear();
-		
-		getAdultsInput().sendKeys(String.valueOf(adults));
-		
-		getChildrenInput().clear();
-		
-		getChildrenInput().sendKeys(String.valueOf(children));
-		
-		for (int i =0; i<children; i++) 
-		{
-			getChildAgeInput(i + 1).clear();
-			getChildAgeInput(i + 1).sendKeys(String.valueOf(childAges[i]));
-		}
+		getAdultsMinusInput().click();
 		
 	}
+	public void setAdultsPlusInput()
+	{
+		getAdultsPlusInput().click();
+		
+	}
+	public void setChildrensMinusInput()
+	{
+		getChildrensMinusInput().click();
+	}
+	public void setChildrensPlusInput()
+	{
+		getChildrensPlusInput().click();
+	}
+	
+	public void setChildrensAgeInput()
+	{
+		getChildAgeInput().click();
+	}
+	
+	public void setAdditionalRooms()
+	{
+		getAdditionalRooms().click();
+	}
+	public void setRemoveAdditionalRoomButton()
+	{
+		getRemoveAdditionalRoomButton().click();
+	}
+	
+	public void setNationalityInput()
+	{
+		getNationalityInput().click();
+	}
+	
+	public void setResidencyInput()
+	{
+		getResidencyInput().click();
+	}
+	public void setIsolationCheckBox()
+	{
+		getIsolationCheckBox().click();
+	}
+	
+
 	
 	public void addSpecialRequests(String specialRequest)
 	{
 		getSpecialRequestInput().sendKeys(specialRequest);
 	}
 	
-	public void searchHotels()
+	public void dropAndEnter() {
+
+			act=new Actions(driver);
+
+			act.build();
+
+			act.keyDown(Keys.ARROW_DOWN).keyUp(Keys.ARROW_DOWN).keyDown(Keys.ENTER).keyUp(Keys.ENTER).perform();
+
+		}
+	
+	public void setSearchHotels()
 	{
 		getSearchButton().click();
 		
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("searchResult")));
+		//WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("searchResult")));
 	}
 	
 }
