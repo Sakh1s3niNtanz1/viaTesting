9be5a77f388f3ab9cb0a2faf9d7c0b5e1a774801
PhilaniMagwaza <149668645+PhilaniMagwaza@users.noreply.github.com>
2023-12-19 10:42:06 +0200

    Update RegistrationPage.java

--- viaTesting/src/main/java/com/via/pages/RegistrationPage.java
+++ viaTesting/src/main/java/com/via/pages/RegistrationPage.java
@@ -1,11 +1,22 @@
 package com.via.pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.via.base.ViaBase;
 
 public class RegistrationPage extends ViaBase {
+	
+	WebDriverWait wait;
+	
+	public void signUpSetUp() {
+		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
+		driver.findElement(By.xpath("//div[@id='SignInContent']/div[1]/div[1]/div[2]/span/label/span")).click();
+	}
 	
 	public WebElement getSignUp() {
 		
@@ -21,45 +32,67 @@ public class RegistrationPage extends ViaBase {
 	
 	
 	public WebElement getCreatAccountBtn() {
-		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
-		driver.findElement(By.xpath("//div[@id='SignInContent']/div[1]/div[1]/div[2]/span/label/span")).click();
+		
 		return driver.findElement(By.id("signUpValidate"));
 		}
 	
 	public WebElement getEmailField() {
-		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
-		driver.findElement(By.xpath("//div[@id='SignInContent']/div[1]/div[1]/div[2]/span/label/span")).click();
+		
 		
 		return driver.findElement(By.id("emailIdSignUp"));
 	}
 	
 	public WebElement getPasswordField() {
-		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
-		driver.findElement(By.xpath("//div[@id='SignInContent']/div[1]/div[1]/div[2]/span/label/span")).click();
+		
 		
 		return driver.findElement(By.id("passwordSignUp"));
 	}
 	
 	public WebElement getNameField() {
-		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
-		driver.findElement(By.xpath("//div[@id='SignInContent']/div[1]/div[1]/div[2]/span/label/span")).click();
+		
 		
 		return driver.findElement(By.id("nameSignUp"));
 	}
 	
 	public WebElement getNumberField() {
-		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
-		driver.findElement(By.xpath("//div[@id='SignInContent']/div[1]/div[1]/div[2]/span/label/span")).click();
+	
 		
 		return driver.findElement(By.id("mobileNoSignUp"));
 	}
 	
 	public WebElement getEmailErrorMsg() {
-		driver.findElement(By.xpath("//div[@id='SignIn']/div")).click();
-		driver.findElement(By.xpath("//div[@id='SignInContent']/div[1]/div[1]/div[2]/span/label/span")).click();
+
 		
-		return driver.findElement(By.xpath("//div[@id='qtip-0-content']"));
+		return driver.findElement(By.xpath("//*[@class=\"qtip-content\"]"));
 	}
 	
+	
+	public WebElement getPasswordErrorMsg() {
+		
+		
+		return driver.findElement(By.xpath("//div[@class='qtip-content']"));
+	}
+	
+	public WebElement getNumberErrorMsg() {
+		
+		
+		return driver.findElement(By.xpath("//div[@class='qtip-content']"));
+	}
+	
+	public WebElement getNameErrorMsg() {
+		
+		
+		return driver.findElement(By.xpath("//div[@class='qtip-content']"));
+	}
+	
+	public WebElement clickCreatAccBtn() {
+		
+		
+		return driver.findElement(By.id("signUpValidate"));
+	}
+	public void waitForEl(WebElement el) {
+		wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(el));
+	}
 
 }
