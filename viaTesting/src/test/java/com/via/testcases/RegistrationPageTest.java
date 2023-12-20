ae9f58fad0f1247627635b7fdfde339f9e289008
PhilaniMagwaza <149668645+PhilaniMagwaza@users.noreply.github.com>
2023-12-20 13:37:07 +0200

    Update RegistrationPageTest.java

--- viaTesting/src/test/java/com/via/testcases/RegistrationPageTest.java
+++ viaTesting/src/test/java/com/via/testcases/RegistrationPageTest.java
@@ -20,6 +20,161 @@ public class RegistrationPageTest {
 	@Test
 	public void clickSigninTest() {
 		
+		rp.clickEl(rp.getSignUp());
+		Assert.assertTrue(rp.checkDisplayEl(rp.getEl("//input[@id='signUpValidate']")));
+
+	
+	}
+	
+	@Test
+	public void validateTitleFieldTest(){
+		rp.signUpSetUp();
+		String expTitle = "Book Flights, Hotels, Bus and Holiday Packages Online - Via.com";
+		String actTitle = rp.getTitle();
+		Assert.assertEquals(actTitle,expTitle);
+	}
+	
+	@Test
+	public void validateCreatAccountBtnTest() {
+		rp.signUpSetUp();
+		Assert.assertTrue(rp.checkDisplayEl(rp.getCreatAccountBtn()));
+	
+	}
+	
+	@Test
+	public void validateEmailFieldTest() {
+		rp.signUpSetUp();
+		Assert.assertTrue(rp.checkDisplayEl(rp.getEmailField()));
+	
+	}
+	
+	@Test
+	public void validatePasswordFieldTest() {
+		rp.signUpSetUp();
+		Assert.assertTrue(rp.checkDisplayEl(rp.getPasswordField()));
+	
+	}
+	
+	@Test
+	public void validateNameFieldTest() {
+		rp.signUpSetUp();
+		Assert.assertTrue(rp.checkDisplayEl(rp.getNameField()));
+	
+	}
+	
+	@Test
+	public void validateNumberFieldTest() {
+		rp.signUpSetUp();
+		Assert.assertTrue(rp.checkDisplayEl(rp.getNumberField()));
+	
+	}
+	
+	@Test
+	public void validateRegistrationTest() {
+		rp.signUpSetUp();
+		rp.sendKeysToEl(rp.getEmailField(), "magwaza749@gmail.com");
+		rp.sendKeysToEl(rp.getPasswordField(), "Philani@123");
+		rp.sendKeysToEl(rp.getNameField(), "Philani Magwaza");
+		rp.dropDownSelect();
+		rp.sendKeysToEl(rp.getNumberField(), "0719270404");
+		rp.clickEl(rp.clickCreatAccBtn());
+		rp.waitForEl(rp.getEl("//div[@id='viaAlert']/div/div"));
+		Assert.assertTrue(rp.checkDisplayEl(rp.getEl("//div[@id='viaAlert']/div/div")));
+
+		
+	}
+	
+	@Test
+	public void validateCountryCodeDropDownd() {
+		rp.signUpSetUp();
+		rp.dropDownSelect();
+		Assert.assertTrue(rp.checkDisplayEl(rp.getEl("//select[@id='mobileIsdSignUp']/option[201]")));
+	
+	}
+	
+	
+	@Test
+	public void validateEmailErrorMsgTest() {
+		rp.signUpSetUp();
+		rp.clickCreatAccBtn().click();
+		rp.waitForEl(rp.getEmailErrorMsg());
+		Assert.assertTrue(rp.checkDisplayEl(rp.getEmailErrorMsg()));
+	
+	}
+	
+	@Test
+	public void validatePasswordErrorMsgTest() {
+		rp.signUpSetUp();
+		rp.sendKeysToEl(rp.getEmailField(), "magwaza749@gmail.com");
+		rp.clickEl(rp.clickCreatAccBtn());;
+		rp.waitForEl(rp.getPasswordErrorMsg());
+
+		Assert.assertTrue(rp.checkDisplayEl(rp.getPasswordErrorMsg()));
+	
+	}
+	
+	@Test
+	public void validateNameErrorMsgTest() {
+		rp.signUpSetUp();
+		rp.sendKeysToEl(rp.getEmailField(), "magwaza749@gmail.com");
+		rp.sendKeysToEl(rp.getPasswordField(), "Philani@123");
+		rp.clickEl(rp.clickCreatAccBtn());;
+		rp.waitForEl(rp.getNameErrorMsg());
+
+		Assert.assertTrue(rp.checkDisplayEl(rp.getNameErrorMsg()));
+	
+	}
+	
+	@Test
+	public void validateNunberErrorMsgTest() {
+		rp.signUpSetUp();
+		rp.sendKeysToEl(rp.getEmailField(), "magwaza749@gmail.com");
+		rp.sendKeysToEl(rp.getPasswordField(), "Philani@123");
+		rp.sendKeysToEl(rp.getNameField(), "Philani Magwaza");
+		rp.clickEl(rp.clickCreatAccBtn());;
+		rp.waitForEl(rp.getNumberErrorMsg());
+
+		Assert.assertTrue(rp.checkDisplayEl(rp.getNumberErrorMsg()));
+	
+	}
+	
+	@Test
+	public void validateCountryCode() {
+		rp.signUpSetUp();
+
+		Assert.assertTrue(rp.getCountryCode().isDisplayed());
+	
+	}
+	
+	
+	@AfterMethod
+	public void closeSetUpLogin() {
+		rp.closeSetUp();
+	}
+
+}
+package com.via.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.via.pages.RegistrationPage;
+
+public class RegistrationPageTest {
+	
+	
+	RegistrationPage rp = new RegistrationPage();
+	
+	@BeforeMethod
+	public void setUpLoginTest() {
+		rp.setUp();
+	}
+	
+	@Test
+	public void clickSigninTest() {
+		
 		rp.getSignUp().click();
 	
 	}
