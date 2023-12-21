6d1236e30246aeb80c98655187da5720e9bc604d
PhilaniMagwaza <149668645+PhilaniMagwaza@users.noreply.github.com>
2023-12-21 14:16:01 +0200

    Update LoginPageTest.java

--- viaTesting/src/test/java/com/via/testcases/LoginPageTest.java
+++ viaTesting/src/test/java/com/via/testcases/LoginPageTest.java
@@ -1,5 +1,7 @@
 package com.via.testcases;
 
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,37 +12,43 @@ import com.via.pages.LoginPage;
 public class LoginPageTest {
 	
 	LoginPage lp = new LoginPage();
+	
 	@BeforeMethod
 	public void setUpLogin() {
-		lp.setUp();
+		try {
+			lp.setUp("");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	
 	@Test
 	public void validateSigninBtn() {
 		lp.clickSignin();
 		Assert.assertTrue(lp.checkDisplayEl(lp.getSigninBtn()));
-	
 	}
+	
 	
 	@Test
 	public void validateClickSigninBtn() {
 		lp.clickSignin();
-
 		lp.clickEl(lp.clickSigninBtn());
 		Assert.assertTrue(lp.checkDisplayEl(lp.getEl("//input[@id='loginValidate']")));
-	
 	}
+	
 	
 	@Test 
 	public void validateUsernameField() {
 		lp.clickSignin();
-
 		Assert.assertTrue(lp.checkDisplayEl(lp.getUsernameField()));
 	}
+	
 	
 	@Test
 	public void validatePasswordField() {
 		lp.clickSignin();
-
 		Assert.assertTrue(lp.checkDisplayEl(lp.getPasswordField()));
 	}
 	
@@ -48,193 +56,58 @@ public class LoginPageTest {
 	@Test
 	public void loginValidattionTest() {
 		lp.clickSignin();
-		lp.getUsernameField().sendKeys("magwaza749@gmail.com");
-		lp.getPasswordField().sendKeys("Philani@123");
+		lp.sendKeysToEl(lp.getUsernameField(), lp.getPropertyValue("email"));
+		lp.sendKeysToEl(lp.getPasswordField(), lp.getPropertyValue("password"));		
 		lp.clickEl(lp.clickSigninBtn());
 		Assert.assertTrue(lp.checkDisplayEl(lp.getGreetingMsg()));
-
 	}
 
 	
 	@Test
 	public void validateForgetPasswordTest() {
 		lp.clickSignin();
-
 		Assert.assertTrue(lp.checkDisplayEl(lp.getForgotPassword()));
-		
-	
 	}
 	
-	
+	@Test
+	public void validateClickForgetPasswordTest() {
+		lp.clickSignin();
+		lp.clickEl(lp.getForgotenPassBTN());
+		Assert.assertTrue(lp.checkDisplayEl(lp.getOTPField()));
+	}
 	
 	@Test
 	public void validateUsernameErrorMsgTest() throws InterruptedException {
 		lp.clickSignin();
 		lp.clickEl(lp.clickSigninBtn());
 		lp.waitForEl(lp.getUsernameErrorMsg());
-		
-		
-
 		Assert.assertTrue(lp.checkDisplayEl(lp.getUsernameErrorMsg()));
-	
 	}
+	
 	
 	@Test
 	public void validatePasswordErrorMsgTest() {
 		lp.clickSignin();
-		lp.getUsernameField().sendKeys("magwaza749@gmail.com");
-
+		lp.sendKeysToEl(lp.getUsernameField(), lp.getPropertyValue("email"));
 		lp.clickEl(lp.clickSigninBtn());
 		lp.waitForEl(lp.getPasswordErrorMsg());
 		Assert.assertTrue(lp.checkDisplayEl(lp.getPasswordErrorMsg()));
-	
 	}
+	
 	
 	@Test
 	public void validateWrongCredentialsErrorMsgTest() {
 		lp.clickSignin();
-		lp.sendKeysToEl(lp.getUsernameField(), "magwaza749@gmail.com");
-		lp.sendKeysToEl(lp.getPasswordField(), "Philani@124");		
+		lp.sendKeysToEl(lp.getUsernameField(), lp.getPropertyValue("email"));
+		lp.sendKeysToEl(lp.getPasswordField(), lp.getPropertyValue("wrongpassword"));		
 		lp.clickEl(lp.clickSigninBtn());
 		lp.waitForEl(lp.getWrongCredentialsErrorMsg());
 		Assert.assertTrue(lp.checkDisplayEl(lp.getWrongCredentialsErrorMsg()));
-	
 	}
-	
-	
-	
 	
 	
 	@AfterMethod
 	public void closeSetUpLogin() {
-		lp.closeSetUp();
+		lp.cleanAndClose();
 	}
-	
-	
-
-}
-package com.via.testcases;
-
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import com.via.pages.LoginPage;
-
-public class LoginPageTest {
-	
-	LoginPage lp = new LoginPage();
-	@BeforeMethod
-	public void setUpLogin() {
-		lp.setUp();
-	}
-	@Test
-	public void validateSigninBtn() {
-		lp.clickSignin();
-		Assert.assertTrue(lp.getSigninBtn().isDisplayed());
-	
-	}
-	
-	@Test
-	public void validateClickSigninBtn() {
-		lp.clickSignin();
-
-		lp.clickSigninBtn().click();
-	
-	}
-	
-	@Test 
-	public void validateUsernameField() {
-		lp.clickSignin();
-
-		Assert.assertTrue(lp.getUsernameField().isDisplayed());
-	}
-	
-	@Test
-	public void validatePasswordField() {
-		lp.clickSignin();
-
-		Assert.assertTrue(lp.getPasswordField().isDisplayed());
-	}
-	
-	@Test
-	public void validateLoginTest() {
-		lp.clickSignin();
-
-		
-		lp.clickSigninBtn().click();
-
-	}
-	
-	@Test
-	public void loginValidattionTest() {
-		lp.clickSignin();
-		lp.getUsernameField().sendKeys("magwaza749@gmail.com");
-		lp.getPasswordField().sendKeys("Philani@123");
-		lp.clickSigninBtn().click();
-		Assert.assertTrue(lp.getGreetingMsg().isDisplayed());
-
-	}
-
-	
-	@Test
-	public void validateForgetPasswordTest() {
-		lp.clickSignin();
-
-		Assert.assertTrue(lp.getForgotPassword().isDisplayed());
-		
-	
-	}
-	
-	
-	
-	@Test
-	public void validateUsernameErrorMsgTest() throws InterruptedException {
-		lp.clickSignin();
-		lp.clickSigninBtn().click();
-//		Thread.sleep(3000);
-		lp.waitForEl(lp.getUsernameErrorMsg());
-		
-		
-
-		Assert.assertTrue(lp.getUsernameErrorMsg().isDisplayed());
-	
-	}
-	
-	@Test
-	public void validatePasswordErrorMsgTest() {
-		lp.clickSignin();
-		lp.getUsernameField().sendKeys("magwaza749@gmail.com");
-
-		lp.clickSigninBtn().click();
-		lp.waitForEl(lp.getPasswordErrorMsg());
-		Assert.assertTrue(lp.getPasswordErrorMsg().isDisplayed());
-	
-	}
-	
-	@Test
-	public void validateWrongCredentialsErrorMsgTest() {
-		lp.clickSignin();
-		lp.getUsernameField().sendKeys("magwaza749@gmail.com");
-		lp.getPasswordField().sendKeys("Philani@124");
-		
-		lp.clickSigninBtn().click();
-		lp.waitForEl(lp.getWrongCredentialsErrorMsg());
-
-		Assert.assertTrue(lp.getWrongCredentialsErrorMsg().isDisplayed());
-	
-	}
-	
-	
-	
-	
-	
-	@AfterMethod
-	public void closeSetUpLogin() {
-		lp.closeSetUp();
-	}
-	
-	
-
 }
